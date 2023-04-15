451832469eaac47ca4fa974c69eb824ac18d9027
AnnaK <anna_morozova_kz@list.ru>
2023-04-15 15:25:47 +0300

    JaCoCo

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxCorrectArray() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 14, 3, 8, 6, 16, 11, 8};
+        long expected = 16;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
